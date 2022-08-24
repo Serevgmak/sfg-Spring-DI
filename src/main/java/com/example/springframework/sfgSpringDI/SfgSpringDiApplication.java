65bd485027aef83c414a467bfc5595d90e2fccc1
Sergei Makarov <sergei.makarov997@gmail.com>
2022-08-24 10:55:56 +0200

    Added project from Spring Initializr

---
+++ src/main/java/com/example/springframework/sfgSpringDI/SfgSpringDiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springframework.sfgSpringDI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgSpringDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgSpringDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springframework/sfgSpringDI/SfgSpringDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springframework.sfgSpringDI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgSpringDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
